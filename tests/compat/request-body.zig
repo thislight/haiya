a62e733a765f7475f8cccbde2cfe1bcf92849e3d
thislight <l1589002388@gmail.com>
2024-09-13 14:22:21 +0800

    tests/request-body: added tests
    * added tests for both .Bandwidth and .Latency
        optimization
    * added tests for unknown length body
        (Transfer-Encoding: chunked in h1)

--- tests/compat/request-body.zig
+++ tests/compat/request-body.zig
@@ -1,37 +1,65 @@
-//! Test if headers (request/response line and headers reading/setting) works
+//! Test if the handler can read request body, under optimized for both .Bandwidth and .Latency.
 const std = @import("std");
 const haiya = @import("haiya");
 const rio = @import("rio");
-const log = std.log.scoped(.h1_headers);
 const curl = @import("curl").Easy;
 const te = std.testing;
 
-fn echoFixedLengthBody(t: *haiya.Transcation) !void {
-    defer t.deinit();
-    if (std.mem.eql(u8, t.request.method, "POST")) {
-        var readerCx = t.bodyReader(.Bandwidth);
-        const reader = readerCx.reader();
-        const text = try reader.readAllAlloc(t.arena(), 4096);
+fn Handlers(comptime readOpt: haiya.Stream.ReadOptimize) type {
+    return struct {
+        fn fixedLength(t: *haiya.Transcation) !void {
+            defer t.deinit();
+            if (std.mem.eql(u8, t.request.method, "POST")) {
+                var readerCx = t.bodyReader(readOpt);
+                const reader = readerCx.reader();
+                const text = try reader.readBoundedBytes(4096);
 
-        _ = t.resetResponse(.OK);
-        var writer = try t.writeBodyStart(
-            .{ .Sized = text.len },
-            t.request.headers.getOne("Content-Type") orelse "text/plain",
-        );
+                _ = t.resetResponse(.OK);
+                var writer = try t.writeBodyStart(
+                    .{ .Sized = text.len },
+                    t.request.headers.getOne("Content-Type") orelse "text/plain",
+                );
 
-        _ = try writer.write(text);
-        try writer.close();
-    } else {
-        _ = t.resetResponse(.@"Bad Request");
-        try t.writeBodyNoContent();
-    }
+                _ = try writer.write(text.constSlice());
+                try writer.close();
+            } else {
+                _ = t.resetResponse(.@"Bad Request");
+                try t.writeBodyNoContent();
+            }
+        }
+
+        fn unknownLength(t: *haiya.Transcation) !void {
+            defer t.deinit();
+            if (std.mem.eql(u8, t.request.method, "POST")) {
+                var readerCx = t.bodyReader(readOpt);
+                const reader = readerCx.reader();
+                var input = try reader.readBoundedBytes(4096);
+
+                _ = t.resetResponse(.OK);
+                var writer = try t.writeBodyStart(
+                    .Infinite,
+                    t.request.headers.getOne("Content-Type") orelse "text/plain",
+                );
+
+                _ = try writer.write(input.constSlice());
+                try writer.close();
+            } else {
+                _ = t.resetResponse(.@"Bad Request");
+                try t.writeBodyNoContent();
+            }
+        }
+    };
 }
 
 const Router = haiya.routers.DefineRouter(std.meta.Tuple(&.{}), .{
-    haiya.routers.Path("/fixed-bandwidth", echoFixedLengthBody),
+    haiya.routers.Path("/fixed-bandwidth", Handlers(.Bandwidth).fixedLength),
+    haiya.routers.Path("/unknown-bandwidth", Handlers(.Bandwidth).unknownLength),
+    haiya.routers.Path("/fixed-latency", Handlers(.Latency).fixedLength),
+    haiya.routers.Path("/unknown-latency", Handlers(.Latency).unknownLength),
+    haiya.routers.Always(haiya.handlers.AlwaysNotFound(.{}).handle),
 });
 
-test "can read fixed-length body" {
+test "can read fixed-length body (.Bandwidth)" {
     var router = Router.init(.{});
     var served = try haiya.Server.Serve(?*anyopaque).create(
         te.allocator,
@@ -46,6 +74,87 @@ test "can read fixed-length body" {
     const alloc = arena.allocator();
     var uri = served.baseUrl();
     uri.path = .{ .raw = "/fixed-bandwidth" };
+    const uriText = try std.fmt.allocPrintZ(alloc, "{}", .{uri});
+
+    var client = try curl.init(alloc, .{});
+    defer client.deinit();
+    const resp = try client.post(uriText, "text/plain", "Hello, World!");
+    defer resp.deinit();
+
+    try te.expect(resp.body != null);
+    const body = resp.body.?;
+    try te.expectEqualStrings("Hello, World!", body.items);
+}
+
+test "can read fixed-length body (.Latency)" {
+    var router = Router.init(.{});
+    var served = try haiya.Server.Serve(?*anyopaque).create(
+        te.allocator,
+        Router.routeOrErr,
+        &router,
+        .{},
+    );
+    defer served.destory();
+
+    var arena = std.heap.ArenaAllocator.init(te.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    var uri = served.baseUrl();
+    uri.path = .{ .raw = "/fixed-latency" };
+    const uriText = try std.fmt.allocPrintZ(alloc, "{}", .{uri});
+
+    var client = try curl.init(alloc, .{});
+    defer client.deinit();
+    const resp = try client.post(uriText, "text/plain", "Hello, World!");
+    defer resp.deinit();
+
+    try te.expect(resp.body != null);
+    const body = resp.body.?;
+    try te.expectEqualStrings("Hello, World!", body.items);
+}
+
+test "can read unknown-length body (.Bandwidth)" {
+    var router = Router.init(.{});
+    var served = try haiya.Server.Serve(?*anyopaque).create(
+        te.allocator,
+        Router.routeOrErr,
+        &router,
+        .{},
+    );
+    defer served.destory();
+
+    var arena = std.heap.ArenaAllocator.init(te.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    var uri = served.baseUrl();
+    uri.path = .{ .raw = "/unknown-bandwidth" };
+    const uriText = try std.fmt.allocPrintZ(alloc, "{}", .{uri});
+
+    var client = try curl.init(alloc, .{});
+    defer client.deinit();
+    const resp = try client.post(uriText, "text/plain", "Hello, World!");
+    defer resp.deinit();
+
+    try te.expect(resp.body != null);
+    const body = resp.body.?;
+    try te.expectEqualStrings("Hello, World!", body.items);
+}
+
+test "can read unknown-length body (.Latency)" {
+    var router = Router.init(.{});
+    var served = try haiya.Server.Serve(?*anyopaque).create(
+        te.allocator,
+        Router.routeOrErr,
+        &router,
+        .{},
+    );
+    defer served.destory();
+
+    var arena = std.heap.ArenaAllocator.init(te.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    var uri = served.baseUrl();
+    uri.path = .{ .raw = "/unknown-latency" };
     const uriText = try std.fmt.allocPrintZ(alloc, "{}", .{uri});
 
     var client = try curl.init(alloc, .{});
